d82c42df22d024eb8572339991d5a9fb3ff94405
jf20541 <jf20541@github.com>
2021-06-04 16:04:48 -0400

    updating data.py with for-loop

--- src/data.py
+++ src/data.py
@@ -1,15 +1,12 @@
+from os import lseek
 import pandas as pd
 from Historic_Crypto import HistoricalData
 
 
 if __name__ == "__main__":
-    btc = HistoricalData(
-        "BTC-USD", 86400, "2016-05-25-00-00", "2021-05-23-00-00"
-    ).retrieve_data()
-    btc = btc["close"]
-    eth = HistoricalData(
-        "ETH-USD", 86400, "2016-05-25-00-00", "2021-05-23-00-00"
-    ).retrieve_data()
-    eth = eth["close"]
-    df = pd.concat([btc, eth], axis=1)
-    df.to_csv("../inputs/train.csv")
+
+    assets = ["BTC-USD", 'ETH-USD']
+    for asset in assets:
+        df = HistoricalData(asset, 86400, "2020-05-25-00-00", "2021-06-01-00-00").retrieve_data()
+        df = df['close']
+        df.to_csv("../inputs/train.csv")
